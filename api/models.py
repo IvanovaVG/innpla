d521cd1460839dad5026ef1bab07ac2d08cca401
IvanovaVG <ivanova5883867@gmail.com>
2023-02-01 10:09:56 +0300

    fixed imports

--- api/models.py
+++ api/models.py
@@ -32,7 +32,7 @@ class CreateUser(BaseModel):
         return value
 
     @validator('surname')
-    def validate_name(cls, value):
+    def validate_surname(cls, value):
         if not LETTER_MATCH_PATTERN.match(value):
             raise HTTPException(status_code=422, detail='Surname should contains only letters')
         return value
