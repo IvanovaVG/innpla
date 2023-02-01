b4d53f03a0d8d40c787e9425361a0b3b8c53dd98
IvanovaVG <ivanova5883867@gmail.com>
2023-02-01 17:17:42 +0300

    added delete user

--- api/handlers.py
+++ api/handlers.py
@@ -1,7 +1,10 @@
-from fastapi import APIRouter, Depends
+from typing import Union
+from uuid import UUID
+
+from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.ext.asyncio import AsyncSession
 
-from api.models import CreateUser, ShowUser
+from api.models import CreateUser, ShowUser, DeletedUser, UpdateUser
 from db.connect import get_db
 from db.crud import UserDAL
 
@@ -26,6 +29,35 @@ async def _create_user(body: CreateUser, db: AsyncSession) -> ShowUser:
             )
 
 
+async def _delete_user(user_id: UUID, db: AsyncSession) -> Union[UUID, None]:
+    async with db as session:
+        async with session.begin():
+            user_dal = UserDAL(session)
+            deleted_id = await user_dal.delete_user(user_id=user_id)
+            return deleted_id
+
+
+async def _change_user_info(user_id: UUID, body: UpdateUser, db: AsyncSession) -> Union[ShowUser, None]:
+    async with db as session:
+        async with session.begin():
+            user_dal = UserDAL(session)
+            deleted_id = await user_dal.delete_user(user_id=user_id)
+            return deleted_id
+
+
 @user_router.post('/', response_model=ShowUser)
 async def create_user(body: CreateUser, db: AsyncSession = Depends(get_db)) -> ShowUser:
     return await _create_user(body=body, db=db)
+
+
+@user_router.delete('/', response_model=DeletedUser)
+async def delete_user(user_id: UUID, db: AsyncSession = Depends(get_db)) -> DeletedUser:
+    deleted_user_id = await _delete_user(user_id=user_id, db=db)
+    if not deleted_user_id:
+        raise HTTPException(status_code=404, detail=f'User with id {user_id} not found')
+    return DeletedUser(deleted_id=deleted_user_id)
+
+
+@user_router.patch('/', response_model=ShowUser)
+async def change_user_info(user_id: UUID, body: UpdateUser, db: AsyncSession = Depends(get_db)) -> ShowUser:
+    update_user_info = body.dict(exclude_none=True)
--- db/crud.py
+++ db/crud.py
@@ -1,5 +1,11 @@
-from db.models import User
+from typing import Union
+from uuid import UUID
+
+from sqlalchemy import and_, update
 from sqlalchemy.ext.asyncio import AsyncSession
+
+from api.models import UpdateUser
+from db.models import User
 
 
 class UserDAL:
@@ -15,3 +21,16 @@ class UserDAL:
         self.db_session.add(new_user)
         await self.db_session.flush()
         return new_user
+
+    async def delete_user(self, user_id: UUID) -> Union[UUID, None]:
+        query = update(User).\
+            where(and_(User.user_id == user_id, User.is_active == True)).\
+            values(is_active=False).\
+            returning(User.user_id)
+        res = await self.db_session.execute(query)
+        deleted_id = res.fetchone()
+        return deleted_id[0] if deleted_id else None
+
+    async def change_user_info(self, user_id: UUID, body: UpdateUser):
+        pass
+
